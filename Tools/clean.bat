69fe3468d0bab058840ddad35aeddf75db182e4a
Evan Collier <evan.collier@digipen.edu>
2017-11-02 16:10:20 -0700

    Evan/build scripts (#79)
    * Added new build scripts to prevent yellow builds
    
    * Modified clean script to also remove artifacts

---
+++ Tools/clean.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM cd /d %~dp0 checks out to the directory of the batch file
+REM this is useful so we can always create the build dir in the right spot
+setlocal
+cd /d %~dp0
+
+cd ..
+
+IF EXIST build rmdir /s /q build
+IF EXIST Output rmdir Output
+
+
