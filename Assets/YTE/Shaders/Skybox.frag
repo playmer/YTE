a265e0519fb603f2e80c312cf788ad7cb0a50134
Joshua T. Fisher <playmer@gmail.com>
2018-01-15 15:26:03 -0800

    Skybox work

--- Assets/YTE/Shaders/Skybox.frag
+++ Assets/YTE/Shaders/Skybox.frag
@@ -3,32 +3,62 @@
 #extension GL_ARB_separate_shader_objects : enable
 #extension GL_ARB_shading_language_420pack : enable
 
-layout (location = 0) in vec3 inTextureCoordinates;
 
+///////////////////////////////////////////////////////////////////////////////
+// Defines
+#define M_PI 3.1415926535897932384626433832795
+
+///////////////////////////////////////////////////////////////////////////////
+// UBO Buffers
+
+// ========================
+// Material Values
 layout (binding = 2) uniform UBOMaterial
 {
-    vec3 mDiffuse;
-    vec3 mAmbient;
-    vec3 mSpecular;
-    vec3 mEmissive;
-    vec3 mTransparent;
-    vec3 mReflective;
+    vec4 mDiffuse;
+    vec4 mAmbient;
+    vec4 mSpecular;
+    vec4 mEmissive;
+    vec4 mTransparent;
+    vec4 mReflective;
     float mOpacity;
     float mShininess;
     float mShininessStrength;
     float mReflectivity;
     float mReflectiveIndex;
     float mBumpScaling;
+    vec2 mPadding;    // not used
 } Material;
 
-layout (binding = 3) uniform samplerCube environmentMap;
 
+///////////////////////////////////////////////////////////////////////////////
+// Samplers
+layout (binding = 3) uniform sampler2D environmentMap;
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Fragment Shader Inputs | Vertex Shader Outputs
+layout (location = 0) in vec3 inTextureCoordinates;
+layout (location = 1) in vec3 inEyeVector;
+
+
+// ========================
+// Output of Fragment
+layout (location = 0) out vec4 outFragColor;
 layout (location = 0) out vec4 outFragColor;
 
+
+// ======================
+// Main:
+// Entry Point of Shader
 void main()
 {
-  //vec3 normal = normalize(inTextureCoordinates);
-  //outFragColor = texture(environmentMap, normal);
-  vec3 add = vec3(1.0f, 1.0f, 1.0f);
-  outFragColor = vec4(add + inTextureCoordinates, 1.0f);
+  vec3 V = normalize(inEyeVector);
+
+  float u = atan(V.z,V.x) / (M_PI * 2) + 0.5;
+  float v = 1.0 - acos(V.y) / M_PI;
+
+  vec2 skyUv = vec2(u, v);
+
+  outFragColor = texture(environmentMap, skyUv);
 }
--- Source/YTE/Graphics/Skybox.cpp
+++ Source/YTE/Graphics/Skybox.cpp
@@ -49,6 +49,7 @@ namespace YTE
 
   Skybox::Skybox(Composition *aOwner, Space *aSpace, RSValue *aProperties)
     : Component(aOwner, aSpace)
+    , mConstructing(true)
   {
     //auto renderer = aSpace->GetEngine()->GetComponent<GraphicsSystem>()->GetRenderer();
     //auto window = aSpace->GetComponent<GraphicsView>()->GetWindow();
@@ -56,7 +57,6 @@ namespace YTE
     mTextureName = "skybox_cubemap.png";
 
     DeserializeByType<Skybox*>(aProperties, this, Skybox::GetStaticType());
-    mConstructing = true;
   }
 
   Skybox::~Skybox()
@@ -75,6 +75,7 @@ namespace YTE
     mOwner->YTERegister(Events::ScaleChanged, this, &Skybox::TransformUpdate);
 
     CreateSkybox();
+    mConstructing = false;
   }
 
 
@@ -104,6 +105,60 @@ namespace YTE
     mUBOModel.mModelMatrix = glm::scale(mUBOModel.mModelMatrix, mTransform->GetScale());
   }
 
+  Submesh CreateSphere(u32 aSubdivisions, const std::string &aTextureName)
+  {
+    float subdivisions = static_cast<float>(aSubdivisions);
+    Submesh sphere;
+    
+    const float pi = glm::pi<float>();
+    const float tau = 2.0f * pi;
+
+    for (u32 i = 0;  i <= aSubdivisions * 2;  i++)
+    {
+      float s = i * tau / (subdivisions * 2.0f);
+      for (u32 j = 0; j <= aSubdivisions; j++)
+      {
+        float t = j * pi / subdivisions;
+        float x = cos(s) * sin(t);
+        float y = sin(s) * sin(t);
+        float z = cos(t);
+
+        Vertex vert;
+        vert.mPosition = glm::vec3{ x, y, z };
+        vert.mNormal   = glm::vec3{ x, y, z };
+        vert.mTextureCoordinates = glm::vec3{ s / (tau), t / pi, 1.0f };
+        vert.mTangent = glm::vec3{ -sin(s), cos(s), 0.0f };
+
+        if ((i > 0) && (j > 0))
+        {
+          u32 a{ (i - 1)*(aSubdivisions + 1) + (j - 1) };
+          u32 b{ (i - 1)*(aSubdivisions + 1) + (j) };
+          u32 c{ (i)*(aSubdivisions + 1) + (j) };
+          u32 d{ (i)*(aSubdivisions + 1) + (j - 1) };
+          
+          sphere.mIndexBuffer.emplace_back(c);
+          sphere.mIndexBuffer.emplace_back(b);
+          sphere.mIndexBuffer.emplace_back(a);
+          
+          sphere.mIndexBuffer.emplace_back(a);
+          sphere.mIndexBuffer.emplace_back(d);
+          sphere.mIndexBuffer.emplace_back(c);
+        }
+
+        sphere.mVertexBuffer.push_back(vert);
+      }
+    }
+
+    sphere.mVertexBufferSize = sphere.mVertexBuffer.size() * sizeof(Vertex);
+    sphere.mIndexBufferSize = sphere.mIndexBuffer.size() * sizeof(u32);
+
+    sphere.mDiffuseMap = aTextureName;
+    sphere.mDiffuseType = TextureViewType::e2D;
+    sphere.mShaderSetName = "Skybox";
+
+    return sphere;
+  }
+
   void Skybox::CreateSkybox()
   {
     if (nullptr != mInstantiatedSkybox)
@@ -114,64 +169,66 @@ namespace YTE
     std::string meshName = "__SkyBox";
     meshName += mTextureName;
 
-    Submesh submesh;
+    //Submesh submesh;
+    //
+    //Vertex vert0;
+    //Vertex vert1;
+    //Vertex vert2;
+    //Vertex vert3;
+    //Vertex vert4;
+    //Vertex vert5;
+    //Vertex vert6;
+    //Vertex vert7;
+    //
+    //vert0.mPosition = glm::vec3(-1.0, -1.0, 1.0);
+    //vert1.mPosition = glm::vec3(1.0, -1.0, 1.0);
+    //vert2.mPosition = glm::vec3(1.0, 1.0, 1.0);
+    //vert3.mPosition = glm::vec3(-1.0, 1.0, 1.0);
+    //
+    //vert4.mPosition = glm::vec3(-1.0, -1.0, -1.0);
+    //vert5.mPosition = glm::vec3(1.0, -1.0, -1.0);
+    //vert6.mPosition = glm::vec3(1.0, 1.0, -1.0);
+    //vert7.mPosition = glm::vec3(-1.0, 1.0, -1.0);
+    //
+    //std::vector<u32> mIndices{
+    //  2, 1, 0,
+    //  0, 3, 2,
+    //
+    //  6, 5, 1,
+    //  1, 2, 6,
+    //  
+    //  3, 0, 4,
+    //  4, 7, 3,
+    //  
+    //  7, 4, 5,
+    //  5, 6, 7,
+    //  
+    //  6, 2, 3,
+    //  3, 7, 6,
+    //  
+    //  1, 5, 4,
+    //  4, 0, 1
+    //};
+    //
+    //submesh.mDiffuseMap = mTextureName;
+    //submesh.mDiffuseType = TextureViewType::eCube;
+    //submesh.mShaderSetName = "Skybox";
+    //
+    //submesh.mVertexBuffer.emplace_back(vert0);
+    //submesh.mVertexBuffer.emplace_back(vert1);
+    //submesh.mVertexBuffer.emplace_back(vert2);
+    //submesh.mVertexBuffer.emplace_back(vert3);
+    //submesh.mVertexBuffer.emplace_back(vert4);
+    //submesh.mVertexBuffer.emplace_back(vert5);
+    //submesh.mVertexBuffer.emplace_back(vert6);
+    //submesh.mVertexBuffer.emplace_back(vert7);
+    //
+    //submesh.mIndexBuffer = std::move(mIndices);
+    //
+    //submesh.mVertexBufferSize = submesh.mVertexBuffer.size() * sizeof(Vertex);
+    //submesh.mIndexBufferSize = submesh.mIndexBuffer.size() * sizeof(u32);
 
-    Vertex vert0;
-    Vertex vert1;
-    Vertex vert2;
-    Vertex vert3;
-    Vertex vert4;
-    Vertex vert5;
-    Vertex vert6;
-    Vertex vert7;
-
-    vert0.mPosition = glm::vec3(-1.0, -1.0, 1.0);
-    vert1.mPosition = glm::vec3(1.0, -1.0, 1.0);
-    vert2.mPosition = glm::vec3(1.0, 1.0, 1.0);
-    vert3.mPosition = glm::vec3(-1.0, 1.0, 1.0);
-
-    vert4.mPosition = glm::vec3(-1.0, -1.0, -1.0);
-    vert5.mPosition = glm::vec3(1.0, -1.0, -1.0);
-    vert6.mPosition = glm::vec3(1.0, 1.0, -1.0);
-    vert7.mPosition = glm::vec3(-1.0, 1.0, -1.0);
-
-    std::vector<u32> mIndices{
-      2, 1, 0,
-      0, 3, 2,
-
-      6, 5, 1,
-      1, 2, 6,
-      
-      3, 0, 4,
-      4, 7, 3,
-      
-      7, 4, 5,
-      5, 6, 7,
-      
-      6, 2, 3,
-      3, 7, 6,
-      
-      1, 5, 4,
-      4, 0, 1
-    };
-
-    submesh.mDiffuseMap = mTextureName;
-    submesh.mDiffuseType = TextureViewType::eCube;
-    submesh.mShaderSetName = "Skybox";
-
-    submesh.mVertexBuffer.emplace_back(vert0);
-    submesh.mVertexBuffer.emplace_back(vert1);
-    submesh.mVertexBuffer.emplace_back(vert2);
-    submesh.mVertexBuffer.emplace_back(vert3);
-    submesh.mVertexBuffer.emplace_back(vert4);
-    submesh.mVertexBuffer.emplace_back(vert5);
-    submesh.mVertexBuffer.emplace_back(vert6);
-    submesh.mVertexBuffer.emplace_back(vert7);
-
-    submesh.mIndexBuffer = std::move(mIndices);
-
-    submesh.mVertexBufferSize = submesh.mVertexBuffer.size() * sizeof(Vertex);
-    submesh.mIndexBufferSize = submesh.mIndexBuffer.size() * sizeof(u32);
+    auto submesh = CreateSphere(128, mTextureName);
 
     std::vector<Submesh> submeshes{ submesh };
 
