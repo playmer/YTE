a5915a13e043ceb9f8c4f334f9a6f6cc26f5e40f
Joshua T. Fisher <playmer@gmail.com>
2018-01-29 17:08:00 -0800

    Worked on cmake automatic folder maker.

--- cmake/YTE_Target_Folder.cmake
+++ cmake/YTE_Target_Folder.cmake
@@ -2,10 +2,21 @@ function(YTE_Target_Folder aTarget aFolder)
   get_target_property(targetLinkLibraries ${aTarget} LINK_LIBRARIES)
   foreach(linkLibrary ${targetLinkLibraries})
     if (TARGET ${linkLibrary})
-      get_target_property(targetFolder ${aTarget} FOLDER) 
-      get_property(targetFolder TARGET ${aTarget} PROPERTY FOLDER)
+      get_target_property(targetFolder ${linkLibrary} FOLDER)
 
-      message(STATUS ${aFolder}/${targetFolder})
+
+      if (targetFolder STREQUAL "targetFolder-NOTFOUND")
+        set(newFolder ${aFolder})
+      else()
+        #set(newFolder ${aFolder}/${targetFolder})
+        set(newFolder ${aFolder})
+      endif()
+
+      Message(STATUS "${linkLibrary} : ${targetFolder} : ${newFolder}")
+
+      set_target_properties(${linkLibrary} PROPERTIES FOLDER ${newFolder}) 
+
+      YTE_Target_Folder(${linkLibrary} ${newFolder}/${linkLibrary})
     endif()
   endforeach()
 endfunction(YTE_Target_Folder)
