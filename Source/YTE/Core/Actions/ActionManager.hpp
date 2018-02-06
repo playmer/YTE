66f00e3b55cc7c716a3b046f7e90351b88771c71
etc597 <etc597@gmail.com>
2018-02-05 22:22:06 -0800

    Some more dank progress

--- Source/YTE/Core/Actions/ActionManager.hpp
+++ Source/YTE/Core/Actions/ActionManager.hpp
@@ -14,9 +14,60 @@ All content (c) 2017 DigiPen  (USA) Corporation, all rights reserved.
 
 namespace YTE
 {
+#define YTENewAction(name)
+
+  struct Action
+  {
+
+  };
+
+  struct Action_Callback : Action
+  {
+
+  };
+
+  struct Group
+  {
+    Group();
+    Group(const Group& group);
+    template <typename T>
+    void Add(float& aValue, float aFinal, float aDuration)
+    {
+
+    }
+    void Call(std::function<void(void)> aCallback);
+    void Delay(float aDuration);
+  private:
+    std::vector<Action*> mActions;
+  };
+
+  struct ActionSequence
+  {
+    ActionSequence();
+    void AddGroup(const Group &group);
+
+    template <typename T>
+    void Add(float& aValue, float aFinal, float aDuration)
+    {
+
+    }
+
+    void Call(std::function<void(void)> aCallback);
+    void Delay(float aDuration);
+  private:
+    std::queue<Group> mGroups;
+  };
+
   class ActionManager : public Component
   {
+  public:
     YTEDeclareType(ActionManager);
+    ActionManager(Composition *aOwner, RSValue *aProperties = nullptr);
+    void AddSequence(Composition *aComposition, const ActionSequence &sequence);
+    void Initialize();
+    void Update(LogicUpdate *aUpdate);
+  private:
+    std::unordered_map<Composition* aOwner, ActionSequence> mSequences;
   };
 
 }
