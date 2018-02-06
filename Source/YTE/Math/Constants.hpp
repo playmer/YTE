a5946bd35903117d03e8b3981aac1ee0b2a8bbae
etc597 <etc597@gmail.com>
2018-02-06 10:57:27 -0800

    HOTFIX for corrupted level files

---
+++ Source/YTE/Math/Constants.hpp
@@ -0,0 +1,22 @@
+/******************************************************************************/
+/*!
+\author Evan T. Collier
+All content (c) 2017 DigiPen  (USA) Corporation, all rights reserved.
+*/
+/******************************************************************************/
+#pragma once
+
+#ifndef YTE_Math_Constants_hpp
+#define YTE_Math_Constants_hpp
+
+#include "YTE/Utilities/Utilities.hpp"
+
+namespace YTE
+{
+  const glm::mat4 mat4Identity( 1.0f, 0.0f, 0.0f, 0.0f,
+                                0.0f, 1.0f, 0.0f, 0.0f,
+                                0.0f, 0.0f, 1.0f, 0.0f,
+                                0.0f, 0.0f, 0.0f, 1.0f );
+}
+
+#endif
