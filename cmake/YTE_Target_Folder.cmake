58ef17d9db7a8f77862f83a0a4dd0bd7a2840d1b
Joshua T. Fisher <playmer@gmail.com>
2018-02-23 18:28:15 -0800

    Working on cmake

--- cmake/YTE_Target_Folder.cmake
+++ cmake/YTE_Target_Folder.cmake
@@ -1,22 +1,23 @@
-function(YTE_Target_Folder aTarget aFolder)
+function(YTE_Target_Folder aTarget aFolderDelimiter)
+  set_target_properties(${aTarget} PROPERTIES FOLDER ${aFolder})
+
   get_target_property(targetLinkLibraries ${aTarget} LINK_LIBRARIES)
   foreach(linkLibrary ${targetLinkLibraries})
     if (TARGET ${linkLibrary})
       get_target_property(targetFolder ${linkLibrary} FOLDER)
 
+      #if (targetFolder STREQUAL "targetFolder-NOTFOUND")
+      #  set(newFolder ${aFolder})
+      #else()
+      #  #set(newFolder ${aFolder}/${targetFolder})
+      #  set(newFolder ${aFolder})
+      #endif()
 
-      if (targetFolder STREQUAL "targetFolder-NOTFOUND")
-        set(newFolder ${aFolder})
-      else()
-        #set(newFolder ${aFolder}/${targetFolder})
-        set(newFolder ${aFolder})
-      endif()
+      Message(STATUS "${linkLibrary} : ${targetFolder} : ${aFolderDelimiter}")
 
-      Message(STATUS "${linkLibrary} : ${targetFolder} : ${newFolder}")
+      set_target_properties(${linkLibrary} PROPERTIES FOLDER ${aFolderDelimiter})
 
-      set_target_properties(${linkLibrary} PROPERTIES FOLDER ${newFolder}) 
-
-      YTE_Target_Folder(${linkLibrary} ${newFolder}/${linkLibrary})
+      YTE_Target_Folder(${linkLibrary} ${aFolderDelimiter}/${linkLibrary})
     endif()
   endforeach()
 endfunction(YTE_Target_Folder)
