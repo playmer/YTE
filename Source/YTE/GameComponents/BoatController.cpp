ffb590b0178f5cd995855260e4b60a6defe1792c
Unknown <ihfdayton@gmail.com>
2018-03-15 12:17:15 -0700

    Started dialogue camera work, tinkered with boat control/physics

--- Source/YTE/GameComponents/BoatController.cpp
+++ Source/YTE/GameComponents/BoatController.cpp
@@ -91,6 +91,8 @@ namespace YTE
     mStartedTurning = false;
     mSoundEmitter = mOwner->GetComponent<WWiseEmitter>();
 
+    mRigidBody->SetDamping(0.9f, 0.9f);
+
       // Cache ids for all sounds used by this component
     auto soundSystem = mSpace->GetEngine()->GetComponent<WWiseSystem>();
 
