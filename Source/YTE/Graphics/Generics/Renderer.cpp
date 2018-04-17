2d62caafcedc181aef31bc7d876af50cc4eeb94a
etc597 <etc597@gmail.com>
2018-04-17 16:47:41 -0700

    Fixed warning

--- Source/YTE/Graphics/Generics/Renderer.cpp
+++ Source/YTE/Graphics/Generics/Renderer.cpp
@@ -293,6 +293,7 @@ namespace YTE
 
     // Not in the futures map, add it.
     mRequestedMeshes[aMeshFile] = mJobSystem->QueueJobThisThread([aMeshFile](JobHandle& handle)->Any {
+      YTEUnusedArgument(handle);
       auto mesh = new Mesh(aMeshFile);
       return Any{ mesh };
     });
@@ -320,6 +321,7 @@ namespace YTE
 
     // Not in the futures map, add it.
     mRequestedMeshes[aFilename] = mJobSystem->QueueJobThisThread([aFilename](JobHandle& handle)->Any {
+      YTEUnusedArgument(handle);
       auto texture = new Texture(aFilename);
       return Any{ texture };
     });
