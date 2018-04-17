58662c82f497c4696038aeed4c51cc6d76a508ea
Andrew Griffin <optic5471@msn.com>
2018-04-17 13:09:52 -0700

    FPS counter is gone!

--- Source/YTE/Core/Engine.cpp
+++ Source/YTE/Core/Engine.cpp
@@ -312,10 +312,10 @@ namespace YTE
 
     if (totalTime >= 0.5)
     {
-      std::array<char, 64> buffer;
-      sprintf_s(buffer.data(), buffer.size(), "FPS: %f", (counter / 0.5));
-
-      aWindow.SetWindowTitle(buffer.data());
+      // FPS counter to the window name 
+      //std::array<char, 64> buffer;
+      //sprintf_s(buffer.data(), buffer.size(), "FPS: %f", (counter / 0.5));
+      //aWindow.SetWindowTitle(buffer.data());
 
       totalTime = 0.0f;
       counter = 0;
