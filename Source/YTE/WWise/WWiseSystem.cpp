d6d110dda2343f5bcc11e1c22947c6660f770f96
Joshua T. Fisher <playmer@gmail.com>
2018-05-07 18:13:33 -0700

    Working on fixing wwise

--- Source/YTE/WWise/WWiseSystem.cpp
+++ Source/YTE/WWise/WWiseSystem.cpp
@@ -6,6 +6,7 @@
  * \copyright All content 2016 DigiPen (USA) Corporation, all rights reserved.
  */
 /******************************************************************************/
+#include <cstdio>
 
 #define UNICODE
 #include "AK/SoundEngine/Common/AkMemoryMgr.h"    // Memory Manager
